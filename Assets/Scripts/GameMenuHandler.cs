12fc3bfe97434f6d01dc78894ac8410b348a23fb
Siddube <prajwal.be.21@gmail.com>
2023-04-25 17:01:53 +0530

    Implement menus functionality

--- Assets/Scripts/GameMenuHandler.cs
+++ Assets/Scripts/GameMenuHandler.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GameMenuHandler : MonoBehaviour
 {
+  [SerializeField] GameObject gameOverUI;
+  [SerializeField] AsteroidSpawner asteroidSpawner;
+
+  public void EndGame()
+  {
+    asteroidSpawner.enabled = false;
+    gameOverUI.SetActive(true);
+  }
   public void PlayGame()
   {
     SceneManager.LoadScene(1);
--- Assets/Scripts/PlayerColiision.cs
+++ Assets/Scripts/PlayerColiision.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerColiision : MonoBehaviour
 {
+  [SerializeField] GameMenuHandler gameMenuHandler;
   public void Crash()
   {
     this.gameObject.SetActive(false);
+    gameMenuHandler.gameObject.SetActive(true);
   }
 
 }
